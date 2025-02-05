8bf757b3274fd64fe7dc221cf12a9d5bc71556f1
abdusalamovio <iskhakabdusalamov@mail.ru>
2025-02-05 20:56:33 +0500

    randomChar

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,16 +12,31 @@ class MarvelService {
     return await result.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const result = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+    return result.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getResource(
+  getCharacter = async (id) => {
+    const result = await this.getResource(
       `${this._apiBase}characters/${id}?&${this._apiKey}`
     );
+    return this._transformCharacter(result.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      id: char.id,
+      name: char.name,
+      description: char.description
+        ? `${char.description.slice(0, 210)}...`
+        : "There is no description for this character",
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
