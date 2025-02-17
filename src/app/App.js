165f7d912ad5288355a85cdd2e8f46fe4e3c2e72
abdusalamovio <iskhakabdusalamov@icloud.com>
2025-02-17 12:06:32 +0500

    react-router

--- src/app/App.js
+++ src/app/App.js
@@ -1,21 +1,22 @@
 import Header from "../components/header/Header";
-
 import MainPage from "../pages/MainPage";
 import ComicsPage from "../pages/ComicsPage";
 import SinglePage from "../pages/SinglePage";
 
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app">
-      <Header />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/comics" element={<ComicsPage />} />
-        <Route path="/comics/:id" element={<SinglePage />} />
-      </Routes>
-    </div>
+    <Router basename="/marvel">
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/comics" element={<ComicsPage />} />
+          <Route path="/comics/:id" element={<SinglePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
